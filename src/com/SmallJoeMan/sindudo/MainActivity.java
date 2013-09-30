8fb48fced34f116eba9230568a59a96327e2d8d4
Joe Smallman <ijsmallman@gmail.com>
2013-09-30 23:29:16 +0100

    Move slider into Java as it runs smoother.

--- src/com/SmallJoeMan/sindudo/MainActivity.java
+++ src/com/SmallJoeMan/sindudo/MainActivity.java
@@ -6,21 +6,53 @@ import android.app.Activity;
 import android.view.Menu;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	TextView diceCount;
+	SeekBar diceSlider;
+	WebView webview;
+	
 	@SuppressLint("SetJavaScriptEnabled")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		WebView webview = (WebView) findViewById(R.id.webView1);
+		diceCount = (TextView) findViewById(R.id.diceCount);
+		diceSlider = (SeekBar) findViewById(R.id.seekBar);
+		
+		diceCount.setText(getString(R.string.dice_count) + "30");
+		
+		webview = (WebView) findViewById(R.id.webView1);
 		WebSettings webSettings = webview.getSettings();
 		webSettings.setJavaScriptEnabled(true);
 		webview.requestFocusFromTouch();
-		//webview.loadDataWithBaseURL("file:///android_asset/",content, "text/html", "utf-8", null );
 		webview.loadUrl("file:///android_asset/webCode.html"); // Can be used in this way too.
+		
+		diceSlider.setOnSeekBarChangeListener( new OnSeekBarChangeListener()
+		{
+			int progressChanged = 0;
+			
+			
+			public void onProgressChanged(SeekBar diceSlider, int progress, boolean fromUser)
+			{
+				progressChanged = progress;
+				diceCount.setText(getString(R.string.dice_count) + Integer.toString(progress));
+			}
+			public void onStartTrackingTouch(SeekBar diceSlider)
+			{
+				// TODO Auto-generated method stub
+			}
+			public void onStopTrackingTouch(SeekBar diceSlider)
+			{
+				webview.loadUrl("javascript:drawChart(\""+Integer.toString(progressChanged)+"\")");
+			}
+		});
+		
 	}
 
 	@Override
